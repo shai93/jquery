9a3a6d107e663cce51a142a38dc8bd9a42fec04a
shailesh.salekar <shailesh.salekar@accionlabs.com>
2020-07-12 17:34:45 +0530

    first commit

---
+++ traversing.js
@@ -0,0 +1,23 @@
+// DOM Traversing
+
+
+$(document).ready(()=>{
+    // $("p").parents("ul").css({
+    //     border:"3px solid red"
+    // })
+
+    // $("ul").children().css({
+    //     border:"3px solid red"
+    // })
+
+    // $("li.second").find("p").css({
+    //     border:"3px solid red"
+    // })
+
+    // $("p").prev().css({
+    //     border:"2px solid red"
+    // })
+
+    $("p").before("<p>Shailesh</p>")
+})
+
